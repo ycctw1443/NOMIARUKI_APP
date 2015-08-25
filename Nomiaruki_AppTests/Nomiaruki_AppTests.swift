6cc7bc6cc7172bf7986e1c0f68dd212927fe31e1
ycctw1443 <ycctw1443@gmai.com>
2015-08-25 15:04:23 +0900

    Initial Commit

---
+++ Nomiaruki_AppTests/Nomiaruki_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Nomiaruki_AppTests.swift
+//  Nomiaruki_AppTests
+//
+//  Created by 和田絢也 on 2015/08/25.
+//  Copyright (c) 2015年 alancodvo. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Nomiaruki_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
