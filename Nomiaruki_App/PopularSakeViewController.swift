e187f87191dde8cbeac533d5ab595b71dafceee6
alancodvo <alancodvo@gmail.com>
2015-09-16 00:34:44 +0900

    人気の日本酒タップ時の画面遷移ViewControllerの作成

---
+++ Nomiaruki_App/PopularSakeViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PopularSakeViewController.swift
+//  Nomiaruki_App
+//
+//  Created by alan-arakawa-yoshihiro on H27/09/16.
+//  Copyright (c) 平成27年 alancodvo. All rights reserved.
+//
+
+import Foundation
