3643546aaa48622d7769a1bcec9c04cceb530354
alancodvo <alancodvo@gmail.com>
2015-09-15 22:36:21 +0900

    テーブル作成(https://sites.google.com/a/gclue.jp/swift-docs/ni-yinki100-ios/uikit/006-uitableviewdeteburuwo-biao-shi)

--- Nomiaruki_App/FirstViewController.swift
+++ Nomiaruki_App/FirstViewController.swift
@@ -6,7 +6,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController, UISearchBarDelegate {
+class FirstViewController: UIViewController, UISearchBarDelegate, UITableViewDelegate, UITableViewDataSource {
     
     init() {
         super.init(nibName: nil, bundle: nil)
@@ -27,6 +27,11 @@ class FirstViewController: UIViewController, UISearchBarDelegate {
     }
     
     private var mySearchBar: UISearchBar!
+    
+    // Tableで使用する配列を設定する
+    private let myItems: NSArray = ["TEST1", "TEST2", "TEST3"]
+    private var myTableView: UITableView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -58,6 +63,28 @@ class FirstViewController: UIViewController, UISearchBarDelegate {
         // 検索バーをViewに追加する.
         self.view.addSubview(mySearchBar)
         
+        // Status Barの高さを取得する.
+        let barHeight: CGFloat = UIApplication.sharedApplication().statusBarFrame.size.height
+        
+        // Viewの高さと幅を取得する.
+        let displayWidth: CGFloat = self.view.frame.width
+        let displayHeight: CGFloat = self.view.frame.height
+        
+        // TableViewの生成する(status barの高さ分ずらして表示).
+        myTableView = UITableView(frame: CGRect(x: 0, y: barHeight, width: displayWidth, height: displayHeight - barHeight))
+        
+        // Cell名の登録をおこなう.
+        myTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+        
+        // DataSourceの設定をする.
+        myTableView.dataSource = self
+        
+        // Delegateを設定する.
+        myTableView.delegate = self
+        
+        // Viewに追加する.
+        self.view.addSubview(myTableView)
+
         }
     
     /*
@@ -79,5 +106,34 @@ class FirstViewController: UIViewController, UISearchBarDelegate {
         super.didReceiveMemoryWarning()
     }
     
+    /*
+    Cellが選択された際に呼び出されるデリゲートメソッド.
+    */
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        println("Num: \(indexPath.row)")
+        println("Value: \(myItems[indexPath.row])")
+    }
     
+    /*
+    Cellの総数を返すデータソースメソッド.
+    (実装必須)
+    */
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myItems.count
+    }
+    
+    /*
+    Cellに値を設定するデータソースメソッド.
+    (実装必須)
+    */
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        // 再利用するCellを取得する.
+        let cell = tableView.dequeueReusableCellWithIdentifier("MyCell", forIndexPath: indexPath) as! UITableViewCell
+        
+        // Cellに値を設定する.
+        cell.textLabel!.text = "\(myItems[indexPath.row])"
+        
+        return cell
+    }
 }
