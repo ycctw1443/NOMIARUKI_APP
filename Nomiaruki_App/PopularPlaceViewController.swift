0e07db58edb7699fb28176d747e133bced8deb6e
ycctw1443 <ycctw1443@gmail.com>
2015-09-27 16:33:46 +0900

    人気の料理、人気の店の専用ページの作成

---
+++ Nomiaruki_App/PopularPlaceViewController.swift
@@ -0,0 +1,49 @@
+//
+//  PopularPlaceViewController.swift
+//  Nomiaruki_App
+//
+//  Created by 和田絢也 on 2015/09/27.
+//  Copyright (c) 2015年 alancodvo. All rights reserved.
+//
+
+import UIKit
+
+class PopularPlaceViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 背景色を設定.
+        self.view.backgroundColor = UIColor.greenColor()
+        
+        // ボタンを作成.
+        let backButton: UIButton = UIButton(frame: CGRectMake(0,0,120,50))
+        backButton.backgroundColor = UIColor.redColor();
+        backButton.layer.masksToBounds = true
+        backButton.setTitle("Back", forState: .Normal)
+        backButton.layer.cornerRadius = 20.0
+        backButton.layer.position = CGPoint(x: self.view.bounds.width/2 , y:self.view.bounds.height-50)
+        backButton.addTarget(self, action: "onClickMyButton:", forControlEvents: .TouchUpInside)
+        self.view.addSubview(backButton);
+    }
+    
+    /*
+    ボタンイベント.
+    */
+    internal func onClickMyButton(sender: UIButton){
+        
+        // 遷移するViewを定義.
+        let myViewController: UIViewController = FirstViewController()
+        
+        // アニメーションを設定.
+        myViewController.modalTransitionStyle = UIModalTransitionStyle.FlipHorizontal
+        
+        // Viewの移動.
+        self.presentViewController(myViewController, animated: true, completion: nil)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
