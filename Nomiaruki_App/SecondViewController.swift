6848edc841165396aa5ccda4dbd7d80fc1a5f882
ycctw1443 <ycctw1443@gmail.com>
2015-09-28 07:46:23 +0900

    タイトルをつけた

--- Nomiaruki_App/SecondViewController.swift
+++ Nomiaruki_App/SecondViewController.swift
@@ -28,6 +28,20 @@ class SecondViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Labelを作成.
+        let myLabel: UILabel = UILabel(frame: CGRectMake(0,0,200,50))
+        // Labelに文字を代入.
+        myLabel.text = "新着の食べ合わせ"
+        // 文字の色を白にする.
+        myLabel.textColor = UIColor.blackColor()
+        // Textを中央寄せにする.
+        myLabel.textAlignment = NSTextAlignment.Center
+        // 配置する座標を設定する.
+        myLabel.layer.position = CGPoint(x: self.view.bounds.width/2,y: 40)
+        // ViewにLabelを追加.
+        self.view.addSubview(myLabel)
+        
     }
     
     override func didReceiveMemoryWarning() {
