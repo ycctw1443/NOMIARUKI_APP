282f7c96f98bdbc79dc86f6f7bc3b1d16c67511b
alancodvo <alancodvo@gmail.com>
2015-09-28 12:06:32 +0900

    CocoaPods使用の準備とCocoaLumberjackというライブラリのインポート(参照：http://qiita.com/yonell/items/dae698f0e3bc30930765)

---
+++ Nomiaruki_App/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  Nomiaruki_App
+//
+//  Created by alan-arakawa-yoshihiro on H27/09/28.
+//  Copyright © 平成27年 alancodvo. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import "CocoaLumberjack.h"
+
+#endif /* Bridging_Header_h */
