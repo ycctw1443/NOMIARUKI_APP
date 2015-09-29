d9a6e37388da07bece5c176dfa29d87f6906ceba
ycctw1443 <ycctw1443@gmail.com>
2015-09-29 10:17:04 +0900

    セカンド、サードのタブページの試作

--- Nomiaruki_App/SecondViewController.swift
+++ Nomiaruki_App/SecondViewController.swift
@@ -6,13 +6,13 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController,UITableViewDelegate , UITableViewDataSource{
     
     init() {
         super.init(nibName: nil, bundle: nil)
         
         // Viewの背景色をGreenに設定する.
-        self.view.backgroundColor = UIColor.greenColor()
+        self.view.backgroundColor = UIColor.whiteColor()
         
         // tabBarItemのアイコンをMoreに、タグを2と定義する.
         self.tabBarItem = UITabBarItem(tabBarSystemItem: UITabBarSystemItem.More, tag: 2)
@@ -26,22 +26,82 @@ class SecondViewController: UIViewController {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
     }
     
+    
+    private let myItems: NSArray = ["久保田","栃尾の油揚げ", "長岡技大第三食堂"]
+    private var myTableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Labelを作成.
-        let myLabel: UILabel = UILabel(frame: CGRectMake(0,0,200,50))
-        // Labelに文字を代入.
-        myLabel.text = "新着の食べ合わせ"
-        // 文字の色を白にする.
-        myLabel.textColor = UIColor.blackColor()
-        // Textを中央寄せにする.
-        myLabel.textAlignment = NSTextAlignment.Center
-        // 配置する座標を設定する.
-        myLabel.layer.position = CGPoint(x: self.view.bounds.width/2,y: 40)
-        // ViewにLabelを追加.
-        self.view.addSubview(myLabel)
         
+        /*
+        テーブルの生成
+        */
+        // Status Barの高さを取得する.
+        let barHeight: CGFloat = UIApplication.sharedApplication().statusBarFrame.size.height
+        
+        // Viewの高さと幅を取得する.
+        let displayWidth: CGFloat = self.view.frame.width
+        let displayHeight: CGFloat = self.view.frame.height
+        
+        
+        // TableViewの生成する(status barの高さ分ずらして表示).
+        myTableView = UITableView(frame: CGRect(x: 0, y: barHeight , width: displayWidth, height: displayHeight - barHeight - 50))
+        
+        // Cell名の登録をおこなう.
+        myTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+        
+        // DataSourceの設定をする.
+        myTableView.dataSource = self
+        
+        // Delegateを設定する.
+        myTableView.delegate = self
+        
+        // Viewに追加する.
+        self.view.addSubview(myTableView)
+        
+    }
+    
+    
+    /*
+    Cellの総数を返すデータソースメソッド.
+    (実装必須)
+    */
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myItems.count
+    }
+    
+    /*
+    Cellの1行のサイズを変更
+    */
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 210
+    }
+    
+    /*
+    Cellに値を設定するデータソースメソッド.
+    (実装必須)
+    */
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        // 再利用するCellを取得する.
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyCell")
+        
+        // Cellに値を設定する.
+        cell.textLabel!.text = "\(myItems[indexPath.row])"
+        cell.textLabel!.font = UIFont.boldSystemFontOfSize(25)
+
+        
+        //背景画像の設定
+        let myImage = UIImage(named: "kubota2.jpg")
+        cell.imageView?.image = myImage
+        
+        //セル選択時にセルが灰色になるのを防ぐ
+        cell.selectedBackgroundView = .None
+        
+        
+        return cell
     }
     
     override func didReceiveMemoryWarning() {
--- Nomiaruki_App/ThirdViewController.swift
+++ Nomiaruki_App/ThirdViewController.swift
@@ -8,13 +8,13 @@
 
 import UIKit
 
-class ThirdViewController: UIViewController {
+class ThirdViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
     init() {
         super.init(nibName: nil, bundle: nil)
         
         // Viewの背景色をGreenに設定する.
-        self.view.backgroundColor = UIColor.greenColor()
+        self.view.backgroundColor = UIColor.whiteColor()
 
         // tabBarItemのアイコンをFavotitesに、タグを3と定義する.
         self.tabBarItem = UITabBarItem(tabBarSystemItem: UITabBarSystemItem.Favorites, tag: 3)
@@ -28,8 +28,62 @@ class ThirdViewController: UIViewController {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
     }
     
+    var myCollectionView : UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // CollectionViewのレイアウトを生成.
+        let layout = UICollectionViewFlowLayout()
+        
+        // Cell一つ一つの大きさ.
+        layout.itemSize = CGSizeMake(50, 50)
+        
+        // Cellのマージン.
+        layout.sectionInset = UIEdgeInsetsMake(16, 16, 32, 16)
+        
+        // セクション毎のヘッダーサイズ.
+        layout.headerReferenceSize = CGSizeMake(100,30)
+        
+        // CollectionViewを生成.
+        myCollectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        
+        // Cellに使われるクラスを登録.
+        myCollectionView.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: "MyCell")
+        
+        myCollectionView.delegate = self
+        myCollectionView.dataSource = self
+        
+        self.view.addSubview(myCollectionView)
+    }
+    
+    /*
+    Cellが選択された際に呼び出される
+    */
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        
+        print("Num: \(indexPath.row)")
+        print("Value:\(collectionView)")
+        
+    }
+    
+    /*
+    Cellの総数を返す
+    */
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    /*
+    Cellに値を設定する
+    */
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell : UICollectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier("MyCell",
+            forIndexPath: indexPath) 
+        
+        cell.backgroundColor = UIColor.orangeColor()
+        return cell
     }
     
     override func didReceiveMemoryWarning() {
