04e65bdd8c504a370a3b0a68172456f1f478fe6a
ycctw1443 <ycctw1443@gmail.com>
2015-09-29 12:40:32 +0900

    おすすめの食べ合わせページの実装

--- Nomiaruki_App/SecondViewController.swift
+++ Nomiaruki_App/SecondViewController.swift
@@ -6,7 +6,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
+class SecondViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource,UICollectionViewDelegateFlowLayout{
     
     init() {
         super.init(nibName: nil, bundle: nil)
@@ -31,20 +31,35 @@ class SecondViewController: UIViewController, UICollectionViewDelegate, UICollec
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        // Labelを作成.
+        let myLabel: UILabel = UILabel(frame: CGRectMake(0,0,200,50))
+        // Labelに文字を代入.
+        myLabel.text = "おすすめの食べ合わせ"
+        // 文字の色を白にする.
+        myLabel.textColor = UIColor.blackColor()
+        // Textを中央寄せにする.
+        myLabel.textAlignment = NSTextAlignment.Center
+        // 配置する座標を設定する.
+        myLabel.layer.position = CGPoint(x: self.view.bounds.width/2,y: 40)
+        // ViewにLabelを追加.
+        self.view.addSubview(myLabel)
+        
+        
         // CollectionViewのレイアウトを生成.
         let layout = UICollectionViewFlowLayout()
         
-        // Cell一つ一つの大きさ.
-        layout.itemSize = CGSizeMake(50, 50)
+        // Cell一つ一つの大きさのデフォルト値
+        layout.itemSize = CGSizeMake(10, 10)
         
         // Cellのマージン.
-        layout.sectionInset = UIEdgeInsetsMake(16, 16, 32, 16)
+        layout.sectionInset = UIEdgeInsetsMake(16, 32, 32, 32)
         
         // セクション毎のヘッダーサイズ.
-        layout.headerReferenceSize = CGSizeMake(100,50)
+        layout.headerReferenceSize = CGSizeMake(0,0)
         
         // CollectionViewを生成.
-        myCollectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        myCollectionView = UICollectionView(frame: CGRectMake(0, 50, self.view.frame.width, self.view.frame.height - 100) , collectionViewLayout: layout)
         
         // Cellに使われるクラスを登録.
         myCollectionView.registerClass(CustomUICollectionViewCell.self, forCellWithReuseIdentifier: "MyCell")
@@ -52,6 +67,9 @@ class SecondViewController: UIViewController, UICollectionViewDelegate, UICollec
         
         myCollectionView.delegate = self
         myCollectionView.dataSource = self
+        
+        myCollectionView.backgroundColor = UIColor.whiteColor()
+        
         
         self.view.addSubview(myCollectionView)
         
@@ -79,34 +97,16 @@ class SecondViewController: UIViewController, UICollectionViewDelegate, UICollec
     */
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
-        // Section毎にCellの総数を変える.
-        switch(section){
-        case 0:
-            return 5
-            
-        case 1:
-            return 8
-            
-        case 2:
-            return 20
-            
-        default:
-            print("error")
-            return 0
-        }
-        
+        return 3
     }
     
-    /*
-    Sectionに値を設定する
-    */
-    func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
-        
-        let headerView = collectionView.dequeueReusableSupplementaryViewOfKind(UICollectionElementKindSectionHeader, withReuseIdentifier: "Section", forIndexPath: indexPath) 
-        
-        headerView.backgroundColor = UIColor.whiteColor()
-        
-        return headerView
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        if indexPath.row % 2 != 0 {
+            return CGSizeMake(40, 40)
+        }else {
+            return CGSizeMake(self.view.frame.size.width/3 , self.view.frame.size.width/3)
+        }
     }
     
     /*
@@ -117,23 +117,26 @@ class SecondViewController: UIViewController, UICollectionViewDelegate, UICollec
         let cell : CustomUICollectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier("MyCell", forIndexPath: indexPath) as! CustomUICollectionViewCell
         
         // Section毎にCellのプロパティを変える.
-        switch(indexPath.section){
-        case 0:
-            cell.backgroundColor = UIColor.redColor()
-            cell.textLabel?.text = "0"
+        //switch(indexPath.section){
+        //case 1:
+            if indexPath.row % 2 != 0{
+                cell.backgroundColor = UIColor.whiteColor()
+                cell.textLabel?.text = "×"
+                cell.textLabel?.textColor = UIColor.blackColor()
+                cell.textLabel?.font = UIFont.boldSystemFontOfSize(60)
+            }else{
+                //背景画像の設定
+                let backgroundImage: UIImage = UIImage(named:"kubota2.jpg")!
+                cell.backgroundView = UIImageView(image: backgroundImage)
+                cell.textLabel?.text = "久保田"
+                cell.textLabel?.textColor = UIColor.whiteColor()
+                cell.textLabel?.font = UIFont.boldSystemFontOfSize(25)
+            }
             
-        case 1:
-            cell.backgroundColor = UIColor.greenColor()
-            cell.textLabel?.text = "1"
-            
-        case 2:
-            cell.backgroundColor = UIColor.blueColor()
-            cell.textLabel?.text = "2"
-            
-        default:
-            print("section error")
-            cell.backgroundColor = UIColor.whiteColor()
-        }
+        //default:
+        //    print("section error")
+        //    cell.backgroundColor = UIColor.whiteColor()
+        //}
         
         return cell
     }
